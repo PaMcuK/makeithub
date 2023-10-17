b7baaf9b91f0c975837bc10d94ae2b90a80d2bb8
PaMcuK <boornlv@gmail.com>
2023-10-17 17:28:16 +0300

    First commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main>
+      <div className='card'>
+      Welcome to <span className='gr-text'>MakeiTHuB</span> - landing page
+      </div>
+    </main>
+  )
+}
